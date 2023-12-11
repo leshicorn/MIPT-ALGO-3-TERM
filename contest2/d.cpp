c9c5fed9cbb1253529c3792672c4d82a622b9328
leshicorn <romanenkov2003@mail.ru>
2023-12-11 23:05:45 +0300

    class Graph added

--- contest2/d.cpp
+++ contest2/d.cpp
@@ -3,71 +3,83 @@
 #include <stack>
 #include <algorithm>
 
-const int MAXN = 20000;
-
-std::vector<int> graph[MAXN + 1];
-std::vector<int> reverseGraph[MAXN + 1];
-std::vector<bool> visited(MAXN + 1, false);
-std::stack<int> resultStack;
-std::vector<int> componentLabels(MAXN + 1, -1);
-int currentLabel = 0;
-
-void dfsVisit(int node) {
-    visited[node] = true;
-    for (int neighbor : graph[node]) {
-        if (!visited[neighbor]) {
-            dfsVisit(neighbor);
-        }
-    }
-    resultStack.push(node);
-}
-
-void dfsAssignLabels(int node) {
-    visited[node] = true;
-    componentLabels[node] = currentLabel;
-    for (int neighbor : reverseGraph[node]) {
-        if (!visited[neighbor]) {
-            dfsAssignLabels(neighbor);
-        }
-    }
-}
-
-void kosaraju(int n) {
-    for (int i = 1; i <= n; ++i) {
-        if (!visited[i]) {
-            dfsVisit(i);
-        }
+class Graph {
+public:
+    Graph(int verticesCount) : verticesCount(verticesCount), graph(verticesCount + 1),
+                    reverseGraph(verticesCount + 1), visited(verticesCount + 1, false),
+                    componentLabels(verticesCount + 1, -1), currentLabel(0) {}
+        
+        void addEdge(int from, int to) {
+        graph[from].push_back(to);
+        reverseGraph[to].push_back(from);
     }
 
-    fill(visited.begin(), visited.end(), false);
+    void kosaraju() {
+        for (int i = 1; i <= verticesCount; ++i) {
+            if (!visited[i]) {
+                dfsVisit(i);
+            }
+        }
 
-    while (!resultStack.empty()) {
-        int node = resultStack.top();
-        resultStack.pop();
+        std::fill(visited.begin(), visited.end(), false);
 
-        if (!visited[node]) {
-            dfsAssignLabels(node);
-            currentLabel++;
+        while (!resultStack.empty()) {
+            int node = resultStack.top();
+            resultStack.pop();
+
+            if (!visited[node]) {
+                dfsAssignLabels(node);
+                currentLabel++;
+            }
         }
     }
-}
+    std::vector<int> componentLabels;
+    std::stack<int> resultStack;
+    int currentLabel;
+
+private:
+    void dfsVisit(int node) {
+        visited[node] = true;
+        for (int neighbor : graph[node]) {
+            if (!visited[neighbor]) {
+                dfsVisit(neighbor);
+            }
+        }
+        resultStack.push(node);
+    }
+    void dfsAssignLabels(int node) {
+        visited[node] = true;
+        componentLabels[node] = currentLabel;
+        for (int neighbor : reverseGraph[node]) {
+            if (!visited[neighbor]) {
+                dfsAssignLabels(neighbor);
+            }
+        }
+    }
+
+    int verticesCount;
+    std::vector<std::vector<int>> graph;
+    std::vector<std::vector<int>> reverseGraph;
+    std::vector<bool> visited;
+};
 
 int main() {
     int n, m;
     std::cin >> n >> m;
 
+    Graph graph(n);
+
     for (int i = 0; i < m; ++i) {
         int u, v;
         std::cin >> u >> v;
-        graph[u].push_back(v);
-        reverseGraph[v].push_back(u); // Добавим обратные рёбра для алгоритма Косараю
+        graph.addEdge(u, v);
     }
 
-    kosaraju(n);
+    graph.kosaraju();
 
-    std::cout << currentLabel << "\n";
+    std::cout << graph.currentLabel << "\n";
     for (int i = 1; i <= n; ++i) {
-        std::cout << componentLabels[i] + 1 << " ";
+        std::cout << graph.componentLabels[i] + 1 << " ";
     }
     std::cout << "\n";
 
