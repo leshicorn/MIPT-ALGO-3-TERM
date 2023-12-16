ea277f9b47f224a2605e6f4e74abe98d65ba0a6d
leshicorn <romanenkov2003@mail.ru>
2023-12-16 06:07:43 +0300

    SOLID-principles fix

--- contest2/d.cpp
+++ contest2/d.cpp
@@ -3,19 +3,16 @@
 #include <stack>
 #include <algorithm>
 
-class Graph {
+class DFS {
 public:
-    Graph(int verticesCount) : verticesCount(verticesCount), graph(verticesCount + 1),
-                    reverseGraph(verticesCount + 1), visited(verticesCount + 1, false),
-                    componentLabels(verticesCount + 1, -1), currentLabel(0) {}
-        
-        void addEdge(int from, int to) {
-        graph[from].push_back(to);
-        reverseGraph[to].push_back(from);
-    }
+    DFS(const std::vector<std::vector<int>>& graph, const std::vector<std::vector<int>>& reverseGraph,
+        std::vector<bool>& visited, std::stack<int>& resultStack, std::vector<int>& componentLabels,
+        int& currentLabel)
+        : graph(graph), reverseGraph(reverseGraph), visited(visited), resultStack(resultStack),
+          componentLabels(componentLabels), currentLabel(currentLabel) {}
 
-    void kosaraju() {
-        for (int i = 1; i <= verticesCount; ++i) {
+    void runDFS() {
+        for (size_t i = 1; i <= graph.size(); ++i) {
             if (!visited[i]) {
                 dfsVisit(i);
             }
@@ -33,11 +30,15 @@ public:
             }
         }
     }
-    std::vector<int> componentLabels;
-    std::stack<int> resultStack;
-    int currentLabel;
 
 private:
+    const std::vector<std::vector<int>>& graph;
+    const std::vector<std::vector<int>>& reverseGraph;
+    std::vector<bool>& visited;
+    std::stack<int>& resultStack;
+    std::vector<int>& componentLabels;
+    int& currentLabel;
+
     void dfsVisit(int node) {
         visited[node] = true;
         for (int neighbor : graph[node]) {
@@ -47,6 +48,7 @@ private:
         }
         resultStack.push(node);
     }
+
     void dfsAssignLabels(int node) {
         visited[node] = true;
         componentLabels[node] = currentLabel;
@@ -56,11 +58,40 @@ private:
             }
         }
     }
+};
 
+class Graph {
+public:
+    Graph(int verticesCount) : verticesCount(verticesCount), graph(verticesCount + 1),
+                              reverseGraph(verticesCount + 1), visited(verticesCount + 1, false),
+                              componentLabels(verticesCount + 1, -1), currentLabel(0) {}
+
+    void addEdge(int from, int to) {
+        graph[from].push_back(to);
+        reverseGraph[to].push_back(from);
+    }
+
+    void kosaraju() {
+        DFS dfs(graph, reverseGraph, visited, resultStack, componentLabels, currentLabel);
+        dfs.runDFS();
+    }
+
+    int getCurrentLabel() const {
+        return currentLabel;
+    }
+
+    const std::vector<int>& getComponentLabels() const {
+        return componentLabels;
+    }
+
+private:
     int verticesCount;
     std::vector<std::vector<int>> graph;
     std::vector<std::vector<int>> reverseGraph;
     std::vector<bool> visited;
+    std::stack<int> resultStack;
+    std::vector<int> componentLabels;
+    int currentLabel;
 };
 
 int main() {
@@ -77,9 +108,9 @@ int main() {
 
     graph.kosaraju();
 
-    std::cout << graph.currentLabel << "\n";
-    for (int i = 1; i <= n; ++i) {
-        std::cout << graph.componentLabels[i] + 1 << " ";
+    std::cout << graph.getCurrentLabel() << "\n";
+    for (int label : graph.getComponentLabels()) {
+        std::cout << label + 1 << " ";
     }
     std::cout << "\n";
 
